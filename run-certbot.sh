7be08f4e50567f5f1a2dabb56bef5bfb7be4db3c
Christopher Landry <38585729+chlandori@users.noreply.github.com>
2025-12-01 23:08:16 -0500

    now export to pfx to windows

--- run-certbot.sh
+++ run-certbot.sh
@@ -20,11 +20,3 @@ done
 certbot certonly \
   --manual --preferred-challenges dns \
   -m "$EMAIL" --agree-tos $DOMAIN_ARGS
-
-# Copy certificates to Windows directory
-WIN_CERT_DIR="/mnt/c/letsencrypt/$(date +%Y%m%d_%H%M%S)"
-mkdir -p "$WIN_CERT_DIR"
-cp -r /etc/letsencrypt/live "$WIN_CERT_DIR/"live
-cp -r /etc/letsencrypt/archive "$WIN_CERT_DIR/archive"
-cp /etc/letsencrypt/renewal/* "$WIN_CERT_DIR/renewal/"
-echo "Certificates copied to $WIN_CERT_DIR"
